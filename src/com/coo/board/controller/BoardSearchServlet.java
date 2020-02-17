b509d07171c3c6aa474edc610fadd24e752a9a4b
ch <ch@DESKTOP-9HOMA6I>
2020-02-17 15:33:19 +0900

    2020_02_17_ch_검색_테스트_필요

--- src/com/coo/board/controller/BoardSearchServlet.java
+++ src/com/coo/board/controller/BoardSearchServlet.java
@@ -3,6 +3,7 @@ package com.coo.board.controller;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 import javax.servlet.ServletException;
@@ -46,47 +47,60 @@ public class BoardSearchServlet extends HttpServlet {
 		Member m = (Member) session.getAttribute("member");
 
 		// search은 검색을 눌렀을시에 자동으로 'true'값을 전해 준다. 즉 검색을 누르지 않으면 null 값이 된다.
-		String check = request.getParameter("search");
-		String title = "";
-		String keyword = "";
-		String date1 = "";
-		String date2 = "";
-		String searchType = "";
+//		String check = request.getParameter("search");
+		String title = request.getParameter("title");
+		String keyword = request.getParameter("keyword");
+		String date1 = request.getParameter("date1");
+		String date2 = request.getParameter("date2");
+		String searchType = request.getParameter("searchType");
 
 		// 검색을 위한 여러 조건의 값을 넘겨 주기 위해 생성
 		Hashtable<String, String> parameters = new Hashtable<String, String>();
-
+		
 		// 검색 버튼을 눌렀는지 아닌지 확인
 		System.out.println("====================== BoardListServlet 테스트 ================================");
-		System.out.println("검색버튼을 눌렀는지 확인 : " + check);
+//		System.out.println("검색버튼을 눌렀는지 확인 : " + check);
 
+		System.out.println(m.toString());
+		
 		// 페이징 처리를 위한 check 값 처리
-
-		// Hashtable parameters 에 값을 넣준다.
-		title = request.getParameter("title");
-		keyword = request.getParameter("keyword");
-		date1 = request.getParameter("date1");
-		date2 = request.getParameter("date2");
-		searchType = request.getParameter("searchType");
-
 		System.out.println("title : " + title);
 		System.out.println("keyword : " + keyword);
 		System.out.println("date1 : " + date1);
 		System.out.println("date2 : " + date2);
 		System.out.println("searchType : " + searchType);
 
+		// Hashtable parameters 에 값을 넣준다.
 		parameters.put("title", title);
-		parameters.put("keyword", keyword);
-		parameters.put("date1", date1);
-		parameters.put("date2", date2);
-		parameters.put("searchType", searchType);
+		if(keyword != null) {
+			parameters.put("keyword", keyword);			
+		}
+		if(date1 != null) {
+			parameters.put("date1", date1);		
+		}
+		if(date2 != null) {
+			parameters.put("date2", date2);		
+		}
+		if(searchType != null) {
+			parameters.put("searchType", searchType);		
+		}else {
+			parameters.put("searchType", "1");
+		}
 		parameters.put("deptView", m.getDeptCode());
+		// 페이징 처리를 위해 필요
+		parameters.put("limit", String.valueOf(limit));
+		
 
-		// 값이 들어 갔는지 확인용
-		System.out.println("==해쉬테이블에 값이 들어 갔는지 확인==");
-		Collection<String> show = parameters.values();
-		for (String out : show) {
-			System.out.println(out);
+		if(m.getEtc() == null) {
+			m.setEtc("");
+		}
+		parameters.put("etc", m.getEtc()); // dao에서 etc로 관리자 와 유저 구분
+		
+		System.out.println("==파라미터 값 확인==");
+		Enumeration<String> out = parameters.keys();
+		while (out.hasMoreElements()) {
+			String keys = out.nextElement();
+			System.out.println(keys + " : " + parameters.get(keys));
 		}
 		System.out.println("==============================");
 
@@ -97,20 +111,39 @@ public class BoardSearchServlet extends HttpServlet {
 		// 사용자의 부서 코드
 		parameters.put("deptView", deptView);
 
-		System.out.println("부서 코드 확인 확인 : " + deptView);
-
 		if (request.getParameter("currentPage") != null) {
 			currentPage = Integer.parseInt(request.getParameter("currentPage"));
 		}
+		
+		// currentPage 값을 계산
+		parameters.put("currentPage", String.valueOf(currentPage));
 
-		// 검색 조건들로 값 가져오기
-		int listCount = (title.equals("")) ? bs.getListCount() : bs.getListCount(parameters);
-
-		System.out.println("listCount(행의 갯수) : " + listCount);
-
-		maxPage = (int) ((double) listCount / limit + 0.9);
-		startPage = ((int) ((double) currentPage / limit + 0.9) - 1) * limit + 1;
-		endPage = startPage + limit - 1;
+		int listCount = 0;
+		list = bs.getListCount(parameters, list);
+		
+		maxPage = Integer.parseInt(parameters.get("maxPage"));
+		startPage = Integer.parseInt(parameters.get("startPage"));
+		endPage = Integer.parseInt(parameters.get("endPage"));
+		
+//		if(title.equals("")) {
+//			listCount = bs.getListCount();
+//			
+//			maxPage = (int) ((double) listCount / limit + 0.9);
+//			startPage = ((int) ((double) currentPage / limit + 0.9) - 1) * limit + 1;
+//			endPage = startPage + limit - 1;
+//		}else {
+//			
+//			list = bs.getListCount(parameters, list);
+//			
+//			maxPage = Integer.parseInt(parameters.get("maxPage"));
+//			startPage = Integer.parseInt(parameters.get("startPage"));
+//			endPage = Integer.parseInt(parameters.get("endPage"));
+//			
+//		}
+		//System.out.println("listCount(행의 갯수) : " + listCount);
+//		maxPage = (int) ((double) listCount / limit + 0.9);
+//		startPage = ((int) ((double) currentPage / limit + 0.9) - 1) * limit + 1;
+//		endPage = startPage + limit - 1;
 
 		System.out.println("maxPage : " + maxPage);
 		System.out.println("startPage : " + startPage);
@@ -119,15 +152,18 @@ public class BoardSearchServlet extends HttpServlet {
 		if (endPage > maxPage) {
 			endPage = maxPage;
 		}
-
-		list = bs.selectList(currentPage, limit, deptView);
-		System.out.println("list(뷰 단에서 출력 할 부서의 행의 갯수) : " + list);
+		
+		// 페이징 처리 하기 전 까지 테스트용
+//		if(title.equals("")) {
+//			list = bs.selectList(currentPage, limit, deptView);
+//		}
 
 		System.out.println("======================================================================");
 		if (list != null) {
 			PageInfo pi = new PageInfo(currentPage, startPage, endPage, listCount, limit, maxPage);
 			request.setAttribute("list", list);
 			request.setAttribute("pi", pi);
+			request.setAttribute("parameters", parameters);
 			request.getRequestDispatcher("views/dept_board/dept_board.jsp").forward(request, response);
 		} else {
 			request.setAttribute("msg", "게시판 조회 실패");
